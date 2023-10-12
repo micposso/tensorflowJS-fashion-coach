a4936d76974fb1e254b551a938980b24378c4f43
micposso <micposso@gmail.com>
2023-10-11 23:13:30 -0400

    finish the webcam detection

--- scripts/script.js
+++ scripts/script.js
@@ -1,168 +1,64 @@
-/**
- * @license
- * Copyright 2018 Google LLC. All Rights Reserved.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =============================================================================
- */
-
-/********************************************************************
- * Demo created by Jason Mayes 2020.
- *
- * Got questions? Reach out to me on social:
- * Twitter: @jason_mayes
- * LinkedIn: https://www.linkedin.com/in/creativetech
- ********************************************************************/
-
-const demosSection = document.getElementById("demos");
-
-var model = undefined;
-
-// Before we can use COCO-SSD class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment to
-// get everything needed to run.
-cocoSsd.load().then(function (loadedModel) {
-  model = loadedModel;
-  // Show demo section now model is ready to use.
-  demosSection.classList.remove("invisible");
-});
-
-/********************************************************************
-// Demo 1: Grab a bunch of images from the page and classify them
-// upon click.
-********************************************************************/
-
-// In this demo, we have put all our clickable images in divs with the
-// CSS class 'classifyOnClick'. Lets get all the elements that have
-// this class.
-const imageContainers = document.getElementsByClassName("classifyOnClick");
-
-// Now let's go through all of these and add a click event listener.
-for (let i = 0; i < imageContainers.length; i++) {
-  // Add event listener to the child element whichis the img element.
-  imageContainers[i].children[0].addEventListener("click", handleClick);
-}
-
-// When an image is clicked, let's classify it and display results!
-function handleClick(event) {
-  if (!model) {
-    console.log("Wait for model to load before clicking!");
-    return;
-  }
-
-  // We can call model.classify as many times as we like with
-  // different image data each time. This returns a promise
-  // which we wait to complete and then call a function to
-  // print out the results of the prediction.
-  model.detect(event.target).then(function (predictions) {
-    // Lets write the predictions to a new paragraph element and
-    // add it to the DOM.
-    console.log(predictions);
-    for (let n = 0; n < predictions.length; n++) {
-      // Description text
-      const p = document.createElement("p");
-      p.innerText =
-        predictions[n].class +
-        " - with " +
-        Math.round(parseFloat(predictions[n].score) * 100) +
-        "% confidence.";
-      // Positioned at the top left of the bounding box.
-      // Height is whatever the text takes up.
-      // Width subtracts text padding in CSS so fits perfectly.
-      p.style =
-        "left: " +
-        predictions[n].bbox[0] +
-        "px;" +
-        "top: " +
-        predictions[n].bbox[1] +
-        "px; " +
-        "width: " +
-        (predictions[n].bbox[2] - 10) +
-        "px;";
-
-      const highlighter = document.createElement("div");
-      highlighter.setAttribute("class", "highlighter");
-      highlighter.style =
-        "left: " +
-        predictions[n].bbox[0] +
-        "px;" +
-        "top: " +
-        predictions[n].bbox[1] +
-        "px;" +
-        "width: " +
-        predictions[n].bbox[2] +
-        "px;" +
-        "height: " +
-        predictions[n].bbox[3] +
-        "px;";
-
-      event.target.parentNode.appendChild(highlighter);
-      event.target.parentNode.appendChild(p);
-    }
-  });
-}
-
-/********************************************************************
-// Demo 2: Continuously grab image from webcam stream and classify it.
-// Note: You must access the demo on https for this to work:
-// https://tensorflow-js-image-classification.glitch.me/
-********************************************************************/
-
 const video = document.getElementById("webcam");
 const liveView = document.getElementById("liveView");
+const demosSection = document.getElementById("demos");
+const enableWebcamButton = document.getElementById("webcamButton");
 
 // Check if webcam access is supported.
-function hasGetUserMedia() {
-  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
+function getUserMediaSupported() {
+  return !!(navigator.mediaDevices &&
+    navigator.mediaDevices.getUserMedia);
 }
-
-// Keep a reference of all the child elements we create
-// so we can remove them easilly on each render.
-var children = [];
 
 // If webcam supported, add event listener to button for when user
-// wants to activate it.
-if (hasGetUserMedia()) {
-  const enableWebcamButton = document.getElementById("webcamButton");
-  enableWebcamButton.addEventListener("click", enableCam);
+// wants to activate it to call enableCam function which we will 
+// define in the next step.
+if (getUserMediaSupported()) {
+  enableWebcamButton.addEventListener('click', enableCam);
 } else {
-  console.warn("getUserMedia() is not supported by your browser");
+  console.warn('getUserMedia() is not supported by your browser');
 }
 
+// Placeholder function for next step. Paste over this in the next step.
 // Enable the live webcam view and start classification.
 function enableCam(event) {
+  // Only continue if the COCO-SSD has finished loading.
   if (!model) {
-    console.log("Wait! Model not loaded yet.");
     return;
   }
-
-  // Hide the button.
-  event.target.classList.add("removed");
-
-  // getUsermedia parameters.
+  
+  // Hide the button once clicked.
+  event.target.classList.add('removed');  
+  
+  // getUsermedia parameters to force video but not audio.
   const constraints = {
-    video: true,
+    video: true
   };
 
   // Activate the webcam stream.
-  navigator.mediaDevices.getUserMedia(constraints).then(function (stream) {
+  navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
     video.srcObject = stream;
-    video.addEventListener("loadeddata", predictWebcam);
+    video.addEventListener('loadeddata', predictWebcam);
   });
 }
 
-// Prediction loop!
+// Store the resulting model in the global scope of our app.
+var model = undefined;
+
+// Before we can use COCO-SSD class we must wait for it to finish
+// loading. Machine Learning models can be large and take a moment 
+// to get everything needed to run.
+// Note: cocoSsd is an external object loaded from our index.html
+// script tag import so ignore any warning in Glitch.
+cocoSsd.load().then(function (loadedModel) {
+  model = loadedModel;
+  // Show demo section now model is ready to use.
+  demosSection.classList.remove('invisible');
+});
+
+var children = [];
+
 function predictWebcam() {
-  // Now let's start classifying the stream.
+  // Now let's start classifying a frame in the stream.
   model.detect(video).then(function (predictions) {
     // Remove any highlighting we did previous frame.
     for (let i = 0; i < children.length; i++) {
@@ -181,19 +77,15 @@ function predictWebcam() {
           " - with " +
           Math.round(parseFloat(predictions[n].score) * 100) +
           "% confidence.";
-        // Draw in top left of bounding box outline.
         p.style =
-          "left: " +
+          "margin-left: " +
           predictions[n].bbox[0] +
-          "px;" +
-          "top: " +
-          predictions[n].bbox[1] +
-          "px;" +
-          "width: " +
+          "px; margin-top: " +
+          (predictions[n].bbox[1] - 10) +
+          "px; width: " +
           (predictions[n].bbox[2] - 10) +
-          "px;";
+          "px; top: 0; left: 0;";
 
-        // Draw the actual bounding box.
         const highlighter = document.createElement("div");
         highlighter.setAttribute("class", "highlighter");
         highlighter.style =
@@ -209,8 +101,6 @@ function predictWebcam() {
 
         liveView.appendChild(highlighter);
         liveView.appendChild(p);
-
-        // Store drawn objects in memory so we can delete them next time around.
         children.push(highlighter);
         children.push(p);
       }
@@ -220,3 +110,4 @@ function predictWebcam() {
     window.requestAnimationFrame(predictWebcam);
   });
 }
+
